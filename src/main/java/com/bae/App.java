6b23e177d91b002d01fd3e70cfb08d969485df26
LiamMcIvor <liam.mcivor1@gmail.com>
2020-01-20 09:01:30 +0000

    build files altered

--- src/main/java/com/bae/App.java
+++ src/main/java/com/bae/App.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App extends SpringBootServletInitializer{
+public class App extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
